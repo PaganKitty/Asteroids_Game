f4534609d9c9a7cdcd748ddffe68a6256d447045
PaganKitty <missa.sokol@gmail.com>
2026-03-01 02:49:16 -0600

    D: modified main.py, added circleshape.py

--- main.py
+++ main.py
@@ -4,19 +4,32 @@ from logger import log_state
 
 def main():
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+
+    dt = 0
     running = True
+
     while running:
-        screen.fill("black")
-        pygame.display.flip()
-        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
 
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        log_state()
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
+
+    pygame.quit()
+  
+    #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
